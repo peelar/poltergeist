052b7bbd188c0b8a0abdea59b69d009283c41949
Adrian Pilarczyk <admin@peelar.dev>
2023-03-02 19:08:29 +0100

    feat: :sparkles: add notion.css

---
+++ poltergeist/styles.ts
@@ -0,0 +1,38 @@
+import type { NotionColor, Text } from "./blocks";
+
+const colorMap: Record<NotionColor, string[]> = {
+  default: ["text-black"],
+  gray: ["text-gray"],
+  brown: ["text-brown"],
+  orange: ["text-orange"],
+  yellow: ["text-yellow"],
+  green: ["text-green"],
+  blue: ["text-blue"],
+  purple: ["text-purple"],
+  pink: ["text-pink"],
+  red: ["text-red"],
+  gray_background: ["color-black", "bg-gray"],
+  brown_background: ["color-black", "bg-brown"],
+  orange_background: ["color-black", "bg-orange"],
+  yellow_background: ["color-black", "bg-yellow"],
+  green_background: ["color-black", "bg-green"],
+  blue_background: ["color-black", "bg-blue"],
+  purple_background: ["color-black", "bg-purple"],
+  pink_background: ["color-black", "bg-pink"],
+  red_background: ["color-black", "bg-red"],
+};
+
+export const matchStylePropsToStyles = (text: Text) => {
+  const styles = text.style;
+  const classes = [
+    ...(styles.bold ? ["font-weight-bold"] : []),
+    ...(styles.italic ? ["font-style-italic"] : []),
+    ...(styles.strikethrough ? ["text-decoration-line-through"] : []),
+    ...(styles.underline ? ["text-decoration-underline"] : []),
+    ...colorMap[styles.color],
+  ];
+
+  const className = classes.join(" ");
+
+  return className;
+};
